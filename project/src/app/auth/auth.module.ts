e08bbc694d5af770905c5082beb28aa55a95680d
SreelakshmiNidhin <sreelakshmi.aitrich@gmail.com>
2023-11-09 02:40:58 +0530

    auth and app module spinner, login modules added

--- project/src/app/auth/auth.module.ts
+++ project/src/app/auth/auth.module.ts
@@ -7,13 +7,17 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterationComponent } from './components/registeration/registeration.component';
 import { SharedModule } from '../shared/shared.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SpinnerComponent } from './components/spinner/spinner.component';
+import { SetPasswordComponent } from './components/set-password/set-password.component';
 
 
 @NgModule({
   declarations: [
     AuthComponent,
     LoginComponent,
-    RegisterationComponent
+    RegisterationComponent,
+    SpinnerComponent,
+    SetPasswordComponent
   ],
   imports: [
     CommonModule,
